65cf4adf69a412259cf2d99224a0a30377b99962
Alaa <alaamohammedgaber@gmail.com>
2023-04-02 21:49:43 +0200

    Revert "Revert "init project with testing  objective Cbridge""
    This reverts commit 1a55b380594aece10ceed571a6910d7c6ef0f4fd. and remove POD files

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Alaa Gaber on 02/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
